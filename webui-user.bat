3831dcad9c891681a05b149a285dc6d8d9af449f
Benevolence Messiah <120072200+BenevolenceMessiah@users.noreply.github.com>
2024-08-18 12:25:33 -0400

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --no-half --no-half-vae --api --opt-split-attention --precision full --port 7861 --autolaunch
 
 call webui.bat
